52773d4d23a8ce98ce03fcaa664391fd45c45b4e
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-01-21 13:07:59 +0200

    setup

--- src/Acting-Improv-Suggestions/acting_improv_suggestion_generator.py
+++ src/Acting-Improv-Suggestions/acting_improv_suggestion_generator.py
@@ -1,3 +1,20 @@
+import random
+import improv_data
+
 
 def generate_improv_suggestion():
-    return 0
+    charAction = (
+        "Nati is "
+        + random.choice(improv_data.emotionDescriptors)
+        + " "
+        + random.choice(improv_data.emotions)
+    )
+    charAction += " That " + random.choice(improv_data.players)
+    charAction += " " + random.choice(improv_data.conjunctions)
+    charAction += " " + random.choice(improv_data.verbs)
+    charAction += " A " + random.choice(improv_data.adjectives)
+    selectedNoun = random.choice(improv_data.nouns)
+    charAction += " " + selectedNoun
+    if "Demon" in selectedNoun:
+        charAction += " (" + random.choice(improv_data.demons) + ")"
+    return charAction
