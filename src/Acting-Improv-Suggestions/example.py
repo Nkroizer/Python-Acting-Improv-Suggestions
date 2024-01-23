57fb5ead459f7644449e5d0659a642c5b122227c
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-01-23 10:40:44 +0200

    Added UI

--- src/Acting-Improv-Suggestions/example.py
+++ src/Acting-Improv-Suggestions/example.py
@@ -1,5 +1,63 @@
 from acting_improv_suggestion_generator import generate_improv_suggestion, Actor
+from tkinter import *
+from tkinter import ttk
 
-yourself = Actor("Nati", "He")
-players = ["Manny", "David", "Self"]
-print(generate_improv_suggestion(yourself, players))
+root = Tk()
+main_actor = StringVar()
+interactee = StringVar()
+sex = IntVar()
+main_actor_obj = Actor("", "")
+interactees = []
+
+
+def add_main_actor_name():
+    main_actor_obj.name = main_actor.get()
+    return True
+
+
+def add_interactee():
+    interactees.append(interactee.get())
+    interactee.set("")
+    return True
+
+
+def generate():
+    sex_var = sex.get()
+    if sex_var == 1:
+        main_actor_obj.pronoun = "He"
+    else:
+        main_actor_obj.pronoun = "She"
+    print(generate_improv_suggestion(main_actor_obj, interactees))
+
+
+frame = Frame(root)
+frame.pack()
+Label(frame, text="Name").grid(row=0, column=0)
+e = Entry(
+    frame,
+    textvariable=main_actor,
+    validate="focusout",
+    validatecommand=add_main_actor_name,
+)
+e.grid(row=0, column=1)
+
+frame4 = Frame(root)
+frame4.pack()
+Radiobutton(frame4, text="Male", variable=sex, value=1).pack(anchor=W)
+Radiobutton(frame4, text="Female", variable=sex, value=2).pack(anchor=W)
+
+# ------------------Frame 2------------------#
+frame2 = Frame(root)
+frame2.pack()
+Label(frame2, text="Add Another Player").grid(row=0, column=0)
+e = Entry(frame2, textvariable=interactee)
+e.grid(row=0, column=1)
+ttk.Button(frame2, text="Add Another", command=add_interactee).grid(row=1, column=0)
+
+# ------------------Frame 3------------------#
+frame3 = Frame(root)
+frame3.pack()
+ttk.Button(frame3, text="Generate", command=generate).grid(row=1, column=0)
+
+
+root.mainloop()
