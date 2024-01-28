2a83262a720dacd66647dc2039c0f63890a7b25f
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-01-28 11:18:42 +0200

    git bash

---
+++ src/Acting-Improv-Suggestions/run_git_save.bat
@@ -0,0 +1,3 @@
+@echo off
+python "C:\Users\Rent\Documents\GitHub\Python-Acting-Improv-Suggestions\src\Acting-Improv-Suggestions\test.py"
+pause
---
+++ src/Acting-Improv-Suggestions/testOutput.py
@@ -0,0 +1,4 @@
+# The man who survived both mustard gas and pepper spray is a seasoned veteran now.
+# A Freudian slip is when you mean one thing and mean your mother.
+# I used to have a handle on life, but then it broke.
+# Adam & Eve were the first ones to ignore the Apple terms and conditions.
