11f279a12c3c1708efcfd82c12add6d07eb83293
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-01-28 10:45:05 +0200

    test output

--- src/Acting-Improv-Suggestions/test.py
+++ src/Acting-Improv-Suggestions/test.py
@@ -6,12 +6,13 @@ COMMIT_MESSAGE = "comment from python script"
 
 def git_push():
     file1 = open("testOutput.py", "a")
+    file1.write("Test")
     file1.close()
-    repo = Repo(PATH_OF_GIT_REPO)
-    repo.git.add(update=True)
-    repo.index.commit(COMMIT_MESSAGE)
-    origin = repo.remote(name="origin")
-    origin.push()
+    # repo = Repo(PATH_OF_GIT_REPO)
+    # repo.git.add(update=True)
+    # repo.index.commit(COMMIT_MESSAGE)
+    # origin = repo.remote(name="origin")
+    # origin.push()
 
 
 git_push()
