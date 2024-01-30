567959991d1e94efd9f8575c4c8ea95de6ba421b
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-01-30 11:25:36 +0200

    comment from python script

--- src/Acting-Improv-Suggestions/testOutput.py
+++ src/Acting-Improv-Suggestions/testOutput.py
@@ -2,4 +2,15 @@
 # A Freudian slip is when you mean one thing and mean your mother.
 # I used to have a handle on life, but then it broke.
 # Adam & Eve were the first ones to ignore the Apple terms and conditions.
-# I threw a boomerang a couple years ago; I know live in constant fear./n# If attacked by a mob of clowns, go for the juggler./n# Build a man a fire and he’ll be warm for a day. Set a man on fire and he’ll be warm for the rest of his life./n# I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels./n# Always borrow money from a pessimist. They’ll never expect it back./n# When the cannibal showed up late to the buffet, they gave him the cold shoulder./n# Are people born with photographic memories, or does it take time to develop?/n# When the cannibal showed up late to the buffet, they gave him the cold shoulder./n# Did you hear about the guy whose whole left side got amputated? He’s all right now./n# A recent study has found that women who carry a little extra weight live longer than the men who mention it./n# My teachers told me I'd never amount to much because I procrastinate so much. I told them, 'Just you wait!'/n# Will glass coffins be a success? Remains to be seen./n
+# I threw a boomerang a couple years ago; I know live in constant fear.
+# If attacked by a mob of clowns, go for the juggler.
+# Build a man a fire and he'll be warm for a day. Set a man on fire and he'll be warm for the rest of his life.
+# I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.
+# Always borrow money from a pessimist. They'll never expect it back.
+# When the cannibal showed up late to the buffet, they gave him the cold shoulder.
+# Are people born with photographic memories, or does it take time to develop?
+# When the cannibal showed up late to the buffet, they gave him the cold shoulder.
+# Did you hear about the guy whose whole left side got amputated? He's all right now.
+# A recent study has found that women who carry a little extra weight live longer than the men who mention it.
+# My teachers told me I'd never amount to much because I procrastinate so much. I told them, 'Just you wait!'
+# Will glass coffins be a success? Remains to be seen.# The problem isn't that obesity runs in your family. It's that no one runs in your family.
