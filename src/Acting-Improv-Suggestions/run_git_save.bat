9c6d2650b51716b9f62d3cc97ff051ab3fa33221
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-01-31 14:44:38 +0200

    auto git fix

--- src/Acting-Improv-Suggestions/run_git_save.bat
+++ src/Acting-Improv-Suggestions/run_git_save.bat
@@ -1,3 +1,3 @@
 @echo off
-python "C:\Users\Rent\Documents\GitHub\Python-Acting-Improv-Suggestions\src\Acting-Improv-Suggestions\test.py"
+python "C:\Users\Rent\Documents\GitHub\Python-Acting-Improv-Suggestions\src\Acting-Improv-Suggestions\auto_git.py"
 pause
