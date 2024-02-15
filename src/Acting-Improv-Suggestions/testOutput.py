18fb4a6f9e990b1f5ac33d382567cad39b48cad9
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-02-15 11:25:46 +0200

    Removed the Multi-core digital summing-amplifier

--- src/Acting-Improv-Suggestions/testOutput.py
+++ src/Acting-Improv-Suggestions/testOutput.py
@@ -50,3 +50,4 @@
 # I was addicted to the hokey pokey, but then I turned myself around.
 # I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.
 # Atheism is a non-prophet organization.
+# I threw a boomerang a couple years ago; I know live in constant fear.
