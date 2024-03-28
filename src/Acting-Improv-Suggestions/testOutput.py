c14fcaa06dee66892c2ef204c3fb28891cb8ffd3
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-03-28 11:25:50 +0200

    Fixed the Offset monitored array

--- src/Acting-Improv-Suggestions/testOutput.py
+++ src/Acting-Improv-Suggestions/testOutput.py
@@ -186,3 +186,4 @@
 # My teachers told me I'd never amount to much because I procrastinate so much. I told them, 'Just you wait!'
 # A blind man walked into a bar… and a table… and a chair…
 # I was wondering why the frisbee kept getting bigger and bigger, but then it hit me.
+# I can tell when people are being judgmental just by looking at them.
