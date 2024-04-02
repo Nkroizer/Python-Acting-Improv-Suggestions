611ea5a99cf0d35209a753364e56f7f3c002dd2c
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-04-02 11:25:40 +0300

    Added the Enhanced current-source

--- src/Acting-Improv-Suggestions/testOutput.py
+++ src/Acting-Improv-Suggestions/testOutput.py
@@ -206,3 +206,4 @@
 # I didn't think orthopedic shoes would help, but I stand corrected.
 # Communist jokes aren't funny unless everyone gets them.
 # I always take life with a grain of salt. And a slice of lemon. And a shot of tequila.
+# My therapist says I have a preoccupation for revenge. We'll see about that.
